a8f1107ed1c55cfb22aef2ee63ebe9fda9fd2651
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-10-13 07:47:55 +0300

    added color scheme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import { EthProvider } from "./contexts/EthContext";
 import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
+import {ThemeProvider} from "@mui/material";
+import {Theme} from "./Theme";
 
 function App() {
-  return (
+
+    return (
     <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro/>
-        </div>
-      </div>
+        <ThemeProvider theme={Theme}>
+              <div id="App">
+                <div className="container">
+                  <Intro/>
+                </div>
+              </div>
+        </ThemeProvider>
     </EthProvider>
   );
 }
