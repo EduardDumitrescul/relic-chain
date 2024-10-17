ad03714afdc1c1ef12fff2fecfa6bf80bf4068d0
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-10-17 13:00:06 +0300

    REL-4: (web) ref: set all variables in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
 import useEth from "./contexts/EthContext/useEth";
 import Intro from "./components/Intro/";
-import {ThemeProvider} from "@mui/material";
+import {ThemeProvider, useTheme} from "@mui/material";
 import {Theme} from "./Theme";
 import React, {useEffect} from "react";
 import {BrowserRouter, createBrowserRouter, Route, Routes} from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import MyRelics from "./components/Home/MyRelics";
+import "./styles.css";
 
 function App() {
     const {state} = useEth();
+    useEffect(() => {
+        const theme = Theme;
+        document.documentElement.style.setProperty("--primary-light", theme.palette.primary.light);
+        document.documentElement.style.setProperty("--primary-main", theme.palette.primary.main);
+        document.documentElement.style.setProperty("--primary-dark", theme.palette.primary.dark);
+        document.documentElement.style.setProperty("--primary-contrast-text", theme.palette.primary.contrastText);
+        document.documentElement.style.setProperty("--secondary-light", theme.palette.secondary.light);
+        document.documentElement.style.setProperty("--secondary-main", theme.palette.secondary.main);
+        document.documentElement.style.setProperty("--secondary-dark", theme.palette.secondary.dark);
+        document.documentElement.style.setProperty("--secondary-contrast-text", theme.palette.secondary.contrastText);
+        document.documentElement.style.setProperty('--spacing-8', theme.spacing(8));
+        document.documentElement.style.setProperty('--spacing-2', theme.spacing(2));
+        document.documentElement.style.setProperty('--spacing-4', theme.spacing(4));
+    }, []);
 
     useEffect(() => {
         if (window.ethereum) {
@@ -40,18 +56,16 @@ function App() {
     return (
         <ThemeProvider theme={Theme}>
               <div id="App">
-                <div className="container">
-                    {state.accounts && state.accounts.length > 0 ? (
-                        <BrowserRouter>
-                            <NavBar/>
-                            <Routes>
-                                <Route path="" element={<p>Home</p>} />
-                            </Routes>
-                        </BrowserRouter>
-                    ) : (
-                        <Intro/>
-                    )}
-                </div>
+                {state.accounts && state.accounts.length > 0 ? (
+                    <BrowserRouter>
+                        <NavBar/>
+                        <Routes>
+                            <Route path="" element={MyRelics()} />
+                        </Routes>
+                    </BrowserRouter>
+                ) : (
+                    <Intro/>
+                )}
               </div>
         </ThemeProvider>
   );
