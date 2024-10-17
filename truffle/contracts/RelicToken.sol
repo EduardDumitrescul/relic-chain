7c14e1c409e64d7dd960474f93dee755852302c2
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-10-17 07:50:40 +0300

    REL-4: (bc) added token count

--- truffle/contracts/RelicToken.sol
+++ truffle/contracts/RelicToken.sol
@@ -2,7 +2,13 @@
 pragma solidity ^0.8.10;
 
 contract RelicToken {
+    uint256 public tokenCount = 0;
+
     constructor(){
 
+    }
+
+    function createToken(address _owner) public {
+        tokenCount ++;
     }
 }
--- truffle/test/relicToken.test.js
+++ truffle/test/relicToken.test.js
@@ -1,8 +1,25 @@
-const relicToken = artifacts.require('RelicToken');
+const RelicToken = artifacts.require('RelicToken');
 
 contract("RelicToken: deployment", () => {
     it("has been deployed", async () => {
-        const token = await relicToken.deployed();
+        const token = await RelicToken.deployed();
         assert(token)
     })
+})
+
+contract("RelicToken: createToken", (accounts) => {
+    let relicToken;
+    const owner = accounts[1];
+
+    beforeEach(async () => {
+        relicToken = await  RelicToken.deployed();
+        await relicToken.createToken(owner)
+    })
+
+    it("increments the token count", async () => {
+        const actual = await relicToken.tokenCount()
+        assert.equal(actual, 1)
+    })
+
+
 })
