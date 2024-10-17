ed2901dfb790fc86d36200768bfe506409f044e0
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-10-17 08:51:09 +0300

    REL-4: (bc) implemented token creation

--- truffle/contracts/RelicToken.sol
+++ truffle/contracts/RelicToken.sol
@@ -1,14 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-contract RelicToken {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract RelicToken is ERC721{
     uint256 public tokenCount = 0;
 
-    constructor(){
+    event TokenCreated(uint256 value);
+
+    constructor()
+    ERC721("RelicToken", "RTK") {
 
     }
 
-    function createToken(address _owner) public {
-        tokenCount ++;
+    function createToken(address owner) public  {
+        tokenCount++;
+        uint256 newTokenId = tokenCount;
+        _safeMint(owner, newTokenId);
+        emit TokenCreated(newTokenId);
     }
 }
--- truffle/test/relicToken.test.js
+++ truffle/test/relicToken.test.js
@@ -9,17 +9,29 @@ contract("RelicToken: deployment", () => {
 
 contract("RelicToken: createToken", (accounts) => {
     let relicToken;
-    const owner = accounts[1];
+    const owner = accounts[1];  // Token owner
 
-    beforeEach(async () => {
-        relicToken = await  RelicToken.deployed();
-        await relicToken.createToken(owner)
-    })
+    describe("when a new token is created", async () => {
+        let tx;
+        beforeEach(async () => {
+            relicToken = await RelicToken.new();
+            tx = await relicToken.createToken(owner);
+        });
 
-    it("increments the token count", async () => {
-        const actual = await relicToken.tokenCount()
-        assert.equal(actual, 1)
-    })
+        it("emits the AccountCreated event", async () => {
+            const expectedEvent = "TokenCreated";
+            const actualEvent = tx.logs[1].event;
+            assert.equal(actualEvent, expectedEvent);
+        })
 
+        it("assigns the token to the correct owner", async () => {
+            const tokenOwner = await relicToken.ownerOf(1);  // Check owner
+            assert.equal(tokenOwner, owner, "Token owner should match the expected owner");
+        });
 
-})
+        it("increments the token count", async () => {
+            const actual = await relicToken.tokenCount();  // Fetch token count
+            assert.equal(actual.toNumber(), 1, "Token count should be 1 after minting");
+        });
+    });
+});
