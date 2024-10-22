d268694fd58da05b6345dd8a7b3c1d4df9c00500
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-10-22 15:19:15 +0300

    REL-10: (web) created page

--- client/src/modules/inventory/InventoryPage.jsx
+++ client/src/modules/inventory/InventoryPage.jsx
@@ -1,16 +1,15 @@
-import styles from './MyRelics.module.css';
-import {Grid, Grid2, useTheme} from "@mui/material";
-import Title from "../../components/Text/Title";
-import RelicCardModel from "./RelicCard/RelicCardModel";
+import {Box, Grid2} from "@mui/material";
 import Headline from "../../components/Text/Headline";
 import RelicCard from "./RelicCard/RelicCard";
 import InventoryService from "./InventoryService";
 import {useEffect, useState} from "react";
 import TokenRetriever from "./TokenRetriever";
 import {useEth} from "../../contexts/EthContext";
+import {Link, useNavigate} from 'react-router-dom';
 
 function InventoryPage() {
     const {state} = useEth();
+    const navigate = useNavigate();
     const inventoryService = new InventoryService(state);
     const [cardModels, setCardModels] = useState([]);
 
@@ -29,10 +28,17 @@ function InventoryPage() {
         fetchCardModels()
     }, []);
 
+    const handleAddRelicClick = () => {
+        navigate("/relic/0");
+    }
+
     console.log('InventoryPage rendered');
     return (
         <>
-            <Headline text="Your Relics" />
+            <Box display="flex" alignItems="center" justifyContent="space-between">
+                <Headline text="Your Relics" />
+                <Link to="/relic/0" className="btn btn-primary">New Relic</Link>
+            </Box>
             <Grid2 container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                 {cardModels.map((model, index) => (
                     <Grid2 size = {4} key={index}>
