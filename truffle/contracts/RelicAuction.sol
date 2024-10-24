65e67dfce2011100081c63f232e7e21c5ca1c90c
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-10-24 08:15:22 +0300

    REL-12: (bc) added token to auction

--- truffle/contracts/RelicAuction.sol
+++ truffle/contracts/RelicAuction.sol
@@ -2,7 +2,21 @@
 pragma solidity ^0.8.10;
 
 contract RelicAuction {
+    uint256[] private auctionedTokenIds;
+
     constructor(){
 
+    }
+
+    function auctionToken(uint256 tokenId)
+    public{
+        auctionedTokenIds.push(tokenId);
+    }
+
+    function getAuctionedTokenIds()
+    public
+    view
+    returns (uint256[] memory) {
+        return auctionedTokenIds;
     }
 }
--- truffle/test/relicAuction.test.js
+++ truffle/test/relicAuction.test.js
@@ -1,8 +1,31 @@
 const RelicAuction = artifacts.require("RelicAuction");
+const RelicToken = artifacts.require("RelicToken");
 
 contract("RelicAuction: deployment", () => {
     it("has been deployed", async () => {
         const auction = await RelicAuction.deployed();
         assert(auction);
     })
+})
+
+contract("RelicAuction: auctionToken", (accounts) => {
+    let token;
+    let auction;
+    const owner = accounts[0];
+    const name = "name";
+    const description = "description";
+
+    beforeEach(async () => {
+        token = await RelicToken.new();
+        await token.createToken(owner, name, description);
+        auction = await RelicAuction.new();
+        await auction.auctionToken(1);
+    })
+
+    it("gets auctioned tokens", async () => {
+        const actual = await auction.getAuctionedTokenIds();
+        const expected = 1;
+        assert.equal(actual, expected);
+    })
+
 })
