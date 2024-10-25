34f21dcb655862f21225983254780f3183eaaf25
Eduard-Valentin Dumitrescul <dumitresculeduard@gmail.com>
2024-10-25 08:05:20 +0300

    REL-3: (web) view auctions

--- client/src/modules/auction/browse/AuctionCard.jsx
+++ client/src/modules/auction/browse/AuctionCard.jsx
@@ -1,7 +1,6 @@
 import {Card, CardContent, CardMedia} from "@mui/material";
 import Title from "../../../components/Text/Title";
 import Body from "../../../components/Text/Body";
-import {useNavigate} from "react-router-dom";
 
 export function AuctionCard(props) {
     const auction = props.model;
@@ -10,12 +9,12 @@ export function AuctionCard(props) {
         <Card sx={{width: "100%"}}>
             <CardMedia
                 sx={{ height: 240 }}
-                image={auction.token.imageSource}
-                title={auction.token.name}
+                image={auction.tokenImageSource}
+                title={auction.tokenName}
             />
             <CardContent sx={{ backgroundColor: "var(--secondary-main)"}}>
-                <Title text={auction.token.name} color="var(--secondary-contrast-text)"/>
-                <Body text={auction.token.description} color="var(--secondary-contrast-text)"/>
+                <Title text={auction.tokenName} color="var(--secondary-contrast-text)"/>
+                <Body text={auction.tokenDescription} color="var(--secondary-contrast-text)"/>
             </CardContent>
         </Card>
     )
